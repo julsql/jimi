0b5192634302123b9f9ca5ef638bb07522307ce7
Jul SQL <julsql1@gmail.com>
2023-10-02 23:29:32 +0200

    link

--- apps/app/lib/api/constants.dart
+++ apps/app/lib/api/constants.dart
@@ -1,7 +1,7 @@
 
 
 class ApiConstants {
-  static String baseUrl = 'http://jimi.h.minet.net';
+  static String baseUrl = 'http://jimi-api.h.minet.net';
   //'http://localhost:8080';
   static String sendMessage = '/chat';
 }
